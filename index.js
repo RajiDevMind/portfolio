81a130b5fa35cd2b091585f314a040bc375b2ca9
Raji DevMind <omoiyaalasso@gmail.com>
2023-09-12 07:57:01 -0700

    dark mode functionalities was setup

--- index.js
+++ index.js
@@ -11,3 +11,31 @@ function animationEffect() {
   }
 }
 animationEffect();
+
+// Dark mode functionality
+const inputEl = document.querySelector(".inputcheckbox");
+const bodyEl = document.querySelector("body");
+
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+
+// function change background color
+const updateChecked = () => {
+  if (inputEl.checked) {
+    bodyEl.style.backgroundColor = "#ccc";
+  } else {
+    bodyEl.style.backgroundColor = "white";
+  }
+};
+updateChecked();
+
+// event update background color
+inputEl.addEventListener("input", () => {
+  updateChecked();
+  updateLocalStorage();
+});
+
+// func to store users settings to local storage
+function updateLocalStorage() {
+  localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+}
+// dark mode func ends here
