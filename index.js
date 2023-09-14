b4b17539126f88b3f91ce9c996b2119303c0a65d
Raji DevMind <omoiyaalasso@gmail.com>
2023-09-14 10:12:13 -0700

    scrollable/draggable functionalyty was added and auto typing effect

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-var i = 0;
-var text =
-  "Welcome! My name is Raji, I'am a Web developer. I focused on Frontend and Backend Engineering!";
-var speed = 60;
+const noteEl = document.querySelector(".notes");
 
-function animationEffect() {
-  if (i < text.length) {
-    document.querySelector(".notes").innerHTML += text.charAt(i);
-    i++;
-    setTimeout(animationEffect, speed);
+const careers = [
+  "Web Developer",
+  "Frontend Engineer",
+  "Backend Engineer",
+  "Content Creator",
+];
+
+let careerIndex = 0;
+
+let characterIndex = 0;
+
+const updateText = () => {
+  characterIndex++;
+  noteEl.innerHTML = `I am a ${careers[careerIndex].slice(0, characterIndex)}`;
+
+  if (characterIndex === careers[careerIndex].length) {
+    careerIndex++;
+    characterIndex = 0;
   }
-}
-animationEffect();
+  if (careerIndex === careers.length) {
+    careerIndex = 0;
+  }
+  setTimeout(updateText, 300);
+};
+updateText();
 
 // Dark mode functionality
 const inputEl = document.querySelector(".inputcheckbox");
@@ -39,3 +53,47 @@ function updateLocalStorage() {
   localStorage.setItem("mode", JSON.stringify(inputEl.checked));
 }
 // dark mode func ends here
+
+// Dialog box popup
+// let popup = document.querySelector("#popup");
+// let displayEl = document.querySelector("#displayPopup");
+// let hideEl = document.querySelector("#hidePopup");
+// let time = 4000;
+// const displayPopup = () => {
+//   popup.classList.add("display");
+// };
+// setTimeout(time, displayPopup());
+// setTimeout(() => {
+//   let users = confirm("Thanks");
+// }, 3000);
+
+const formEl = document.getElementById("myForm");
+const danger = document.querySelector(".danger");
+
+formEl.addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(formEl);
+
+  const checkEmptyValues = [...formData.values()];
+  const isEmpty = checkEmptyValues.includes("");
+  if (isEmpty) {
+    alert("Kindly provide valid details!");
+    console.log("Kindly provide valid details!");
+    return;
+  }
+  const email = formData.get("email");
+
+  const response = await fetch("http://localhost:4500/subscribe", {
+    method: "POST",
+    body: JSON.stringify(email),
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+  });
+  if (response.status === 200) {
+    alert("Subscribed Successfully!");
+  } else {
+    console.log("Failed to Subscribe, Try again!");
+  }
+  e.currentTarget.reset();
+});
